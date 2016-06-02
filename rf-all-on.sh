141f07ebb32e68d410a3157b8dc8f7defbbd7b70
Jack Sorrells <jrs08d@acu.edu>
2016-06-01 20:48:03 -0500

    removed extra /

--- rf-all-on.sh
+++ rf-all-on.sh
@@ -1,4 +1,4 @@
-#!/bin/bash/
+#!/bin/bash
 
 sudo /var/www/rfoutlet/codesend 4281651 -l 176 -p 0
 sudo /var/www/rfoutlet/codesend 4281796 -l 176 -p 0
--- rf-switch-2-off.sh
+++ rf-switch-2-off.sh
@@ -1,4 +1,4 @@
-#!/bin/bash/
+#!/bin/bash
 
 sudo /var/www/rfoutlet/codesend 4281804 -l 176 -p 0
 sudo /var/www/rfoutlet/codesend 4281804 -l 176 -p 0
