fd75efc741346912723c648cd09fe0d512afc6ec
Jack Sorrells <jrs08d@acu.edu>
2016-06-04 15:04:43 -0500

    New switches

---
+++ rf-switch-4-off.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo /var/www/rfoutlet/codesend 1381692 -l 181 -p 0
+sudo /var/www/rfoutlet/codesend 1381692 -l 181 -p 0
+sudo /var/www/rfoutlet/codesend 1381692 -l 181 -p 0
+sudo /var/www/rfoutlet/codesend 1381692 -l 181 -p 0
+sudo /var/www/rfoutlet/codesend 1381692 -l 181 -p 0
---
+++ rf-switch-6-on.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo /var/www/rfoutlet/codesend 1382147 -l 181 -p 0
+sudo /var/www/rfoutlet/codesend 1382147 -l 181 -p 0
+sudo /var/www/rfoutlet/codesend 1382147 -l 181 -p 0
+sudo /var/www/rfoutlet/codesend 1382147 -l 181 -p 0
+sudo /var/www/rfoutlet/codesend 1382147 -l 181 -p 0
