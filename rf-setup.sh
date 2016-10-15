ef2d95a79a3dc99908bc46fed67a731e0da961a5
Jack Sorrells <jacksorrells@me.com>
2016-10-14 21:01:48 -0500

    Let's see if this setup file works

---
+++ rf-setup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo chmod +x rf-switch-1-on.sh rf-switch-1-off.sh rf-switch-2-on.sh rf-switch-2-off.sh rf-switch-3-on.sh rf-switch-3-off.sh rf-switch-4-on.sh rf-switch-4-off.sh rf-switch-5-on.sh rf-switch-5-off.sh rf-switch-6-on.sh rf-switch-6-off.sh rf-lights-on.sh rf-lights-off.sh rf-all-on.sh rf-all-off.sh
